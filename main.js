16277105f0eb45c637b2ad982336ae7cd9e93c08
anapluvcatt <anapstok01@gmail.com>
2026-03-10 07:27:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -152,6 +152,7 @@ const masterAdmin = [
   { name: 'Lex', link: 'https://wa.me/6285830325641' },
   { name: 'Ian Store', link: 'https://wa.me/6285332634927' },
   { name: 'Bang N1k', link: 'https://wa.me/6287859030934' },
+{ name: 'Rrq Dricaz', link: 'https://wa.me/628134875623' },
   { name: 'Bintg', link: 'https://wa.me/6282312376494' }
 ];
 
